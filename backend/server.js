3492d64ebcbce95185e89cd4ed3545ba194c54d5
developerAyesha <149559723+developerAyesha@users.noreply.github.com>
2025-07-17 00:07:09 +0500

    Setup MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('URL Shortener API Running'));
