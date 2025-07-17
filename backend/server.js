c117de9ea6d9f2e3aaa5cde39f43c305f8147955
developerAyesha <149559723+developerAyesha@users.noreply.github.com>
2025-07-17 08:05:04 +0500

    Refactor: move MongoDB connection to config file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-app.use(cors());
+
 const urlRoutes = require('./routes/url.js');
 require('dotenv').config();
 const app = express();
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(express.json());
 
 app.use('/', urlRoutes);
+
 
 
 
